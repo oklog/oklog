fd357e20386b19d60643d052b9eee8dd320a1321
Peter Bourgon <peter@bourgon.org>
2017-01-07 17:33:59 +0100

    Merge query_* files to single query.go
    At the same time...
    
    - Remove QueryEngine and query{Naive,Ripgrep} paths
    - Remove now-useless Records{Queried,Matched} stats
    - Check mergeRecords errs when merging QueryResults

--- pkg/store/query.go
+++ pkg/store/query.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"bytes"
 	"io"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/djherbis/buffer"
@@ -12,6 +15,77 @@ import (
 
 	"github.com/oklog/prototype/pkg/fs"
 	"github.com/oklog/ulid"
+)
+
+// QueryResult contains statistics about, and matching records for, a query.
+type QueryResult struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+	Q    string `json:"q"`
+
+	NodesQueried    int `json:"nodes_queried"`
+	SegmentsQueried int `json:"segments_queried"`
+	ErrorCount      int `json:"error_count,omitempty"`
+
+	Records io.ReadCloser // TODO(pb): audit to ensure closing is valid throughout
+}
+
+// DecodeFrom decodes the QueryResult from the HTTP response.
+func (qr *QueryResult) DecodeFrom(resp *http.Response) {
+	qr.From = resp.Header.Get(httpHeaderFrom)
+	qr.To = resp.Header.Get(httpHeaderTo)
+	qr.Q = resp.Header.Get(httpHeaderQ)
+	qr.NodesQueried, _ = strconv.Atoi(resp.Header.Get(httpHeaderNodesQueried))
+	qr.SegmentsQueried, _ = strconv.Atoi(resp.Header.Get(httpHeaderSegmentsQueried))
+	qr.Records = resp.Body
+}
+
+// EncodeTo encodes the QueryResult to the HTTP response writer.
+func (qr *QueryResult) EncodeTo(w http.ResponseWriter) {
+	w.Header().Set(httpHeaderFrom, qr.From)
+	w.Header().Set(httpHeaderTo, qr.To)
+	w.Header().Set(httpHeaderQ, qr.Q)
+	w.Header().Set(httpHeaderNodesQueried, strconv.Itoa(qr.NodesQueried))
+	w.Header().Set(httpHeaderSegmentsQueried, strconv.Itoa(qr.SegmentsQueried))
+	if qr.Records != nil {
+		io.Copy(w, qr.Records) // TODO(pb): CopyBuffer
+		qr.Records.Close()
+	}
+}
+
+// Merge the other QueryResult into this one.
+func (qr *QueryResult) Merge(other QueryResult) error {
+	qr.NodesQueried += other.NodesQueried
+	qr.SegmentsQueried += other.SegmentsQueried
+	qr.ErrorCount += other.ErrorCount
+
+	// TODO(pb): error handling during mergeRecords
+	var (
+		buf bytes.Buffer
+		err error
+	)
+	switch {
+	case qr.Records != nil && other.Records == nil:
+		defer qr.Records.Close()
+		_, _, _, err = mergeRecords(&buf, qr.Records)
+	case qr.Records == nil && other.Records != nil:
+		defer other.Records.Close()
+		_, _, _, err = mergeRecords(&buf, other.Records)
+	case qr.Records != nil && other.Records != nil:
+		defer qr.Records.Close()
+		defer other.Records.Close()
+		_, _, _, err = mergeRecords(&buf, qr.Records, other.Records)
+	}
+	qr.Records = ioutil.NopCloser(&buf)
+	return err
+}
+
+const (
+	httpHeaderFrom            = "X-OKLog-From"
+	httpHeaderTo              = "X-OKLog-To"
+	httpHeaderQ               = "X-OKLog-Q"
+	httpHeaderNodesQueried    = "X-OKLog-Nodes-Queried"
+	httpHeaderSegmentsQueried = "X-OKLog-Segments-Queried"
 )
 
 type recordFilter func([]byte) bool
--- pkg/store/query_test.go
+++ pkg/store/query_test.go
@@ -2,101 +2,287 @@ package store
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
-	"sync"
 	"testing"
-	"time"
 
-	"github.com/oklog/prototype/pkg/fs"
+	"github.com/oklog/ulid"
 )
 
-func TestQueryEngineNaïve(t *testing.T)   { testQueryEngine(t, QueryEngineNaïve) }
-func TestQueryEngineRipgrep(t *testing.T) { testQueryEngine(t, QueryEngineRipgrep) }
-func TestQueryEngineLazy(t *testing.T)    { testQueryEngine(t, QueryEngineLazy) }
+func TestBatchSegments(t *testing.T) {
+	t.Parallel()
 
-func testQueryEngine(t *testing.T, engine QueryEngine) {
-	storeRoots := os.Getenv("STORE_ROOTS")
-	if storeRoots == "" {
-		t.Skip("skipping; set STORE_ROOTS to run")
+	for _, testcase := range []struct {
+		name  string
+		input []string
+		want  [][]string
+	}{
+		{
+			"no elements",
+			[]string{},
+			[][]string{},
+		},
+		{
+			"one element",
+			[]string{"A-Z"},
+			[][]string{{"A-Z"}},
+		},
+		{
+			"no overlap",
+			[]string{"A-M", "N-Z"},
+			[][]string{{"A-M"}, {"N-Z"}},
+		},
+		{
+			"basic overlap",
+			[]string{"A-M", "N-R", "O-S", "W-Z"},
+			[][]string{{"A-M"}, {"N-R", "O-S"}, {"W-Z"}},
+		},
+		{
+			"complete staggering overlap",
+			[]string{"A-M", "L-R", "O-S", "Q-Z"},
+			[][]string{{"A-M", "L-R", "O-S", "Q-Z"}},
+		},
+		{
+			"mostly overlap",
+			[]string{"A-M", "L-R", "O-S", "W-Z"},
+			[][]string{{"A-M", "L-R", "O-S"}, {"W-Z"}},
+		},
+		{
+			"equality should count as overlap",
+			[]string{"A-B", "B-C", "C-D", "E-F", "F-G"},
+			[][]string{{"A-B", "B-C", "C-D"}, {"E-F", "F-G"}},
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			if want, have := testcase.want, batchSegments(testcase.input); !reflect.DeepEqual(want, have) {
+				t.Fatalf("want %v, have %v", want, have)
+			}
+		})
 	}
-
-	var logs []Log
-	for _, root := range strings.Fields(storeRoots) {
-		log, err := NewFileLog(fs.NewRealFilesystem(false), root, 1000000)
-		if err != nil {
-			t.Fatalf("%s: %v", root, err)
-		}
-		logs = append(logs, log)
-	}
-
-	var (
-		from, _   = time.Parse(time.RFC3339, "2017-01-03T00:36:01+01:00")
-		to, _     = time.Parse(time.RFC3339, "2017-01-04T00:38:59+01:00")
-		q         = "7A(0|1)"
-		statsOnly = false
-	)
-
-	scatterQuery(t, logs, engine, from, to, q, statsOnly)
 }
 
-func scatterQuery(t *testing.T, logs []Log, engine QueryEngine, from, to time.Time, q string, statsOnly bool) {
+func TestMergeReader(t *testing.T) {
+	t.Parallel()
+
 	var (
-		overallBegin = time.Now()
-		resultc      = make(chan QueryResult, len(logs))
-		errc         = make(chan error, len(logs))
+		u100 = ulid.MustNew(100, nil).String()
+		u150 = ulid.MustNew(150, nil).String()
+		u200 = ulid.MustNew(200, nil).String()
+		u250 = ulid.MustNew(250, nil).String()
+		u300 = ulid.MustNew(300, nil).String()
+		u350 = ulid.MustNew(350, nil).String()
+		u400 = ulid.MustNew(400, nil).String()
+		u450 = ulid.MustNew(450, nil).String()
+		u500 = ulid.MustNew(500, nil).String()
 	)
-	for _, log := range logs {
-		go func(log Log) {
-			if res, err := log.Query(engine, from, to, q, statsOnly); err != nil {
-				errc <- err
-			} else {
-				resultc <- res
+	for _, testcase := range []struct {
+		name  string
+		input [][]string
+		want  []string
+	}{
+		{
+			"no elements",
+			[][]string{},
+			[]string{},
+		},
+		{
+			"one element",
+			[][]string{{u150}},
+			[]string{u150},
+		},
+		{
+			"simple merge",
+			[][]string{{u100, u200, u300}, {u150, u250, u350}},
+			[]string{u100, u150, u200, u250, u300, u350},
+		},
+		{
+			"complex merge",
+			[][]string{{u100, u200, u300}, {u250, u350, u450}, {u150, u400, u500}},
+			[]string{u100, u150, u200, u250, u300, u350, u400, u450, u500},
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			// Convert string slice to a set of readers.
+			readers := make([]io.Reader, len(testcase.input))
+			for i, s := range testcase.input {
+				segment := strings.Join(s, "\n") + "\n"
+				readers[i] = strings.NewReader(segment)
 			}
-		}(log)
-	}
 
-	var results []QueryResult
-	for range logs {
-		select {
-		case res := <-resultc:
-			results = append(results, res)
-		case err := <-errc:
-			t.Fatalf("Got error: %v", err)
-		}
-	}
+			// Construct the merge reader from the set of readers.
+			r, err := newMergeReader(readers)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	t.Logf("Query phase complete in %s", time.Since(overallBegin))
-
-	var wg sync.WaitGroup
-	wg.Add(len(results))
-	for _, res := range results {
-		if res.Records == nil {
-			t.Logf("nil Records?")
-			continue
-		}
-		go func(r io.Reader) {
-			defer wg.Done()
-			var (
-				readBegin = time.Now()
-				count     = 0
-				bytes     = 0
-			)
+			// Take lines from the merge reader until EOF.
+			have := []string{}
 			s := bufio.NewScanner(r)
 			for s.Scan() {
-				count++
-				bytes += len(s.Bytes())
+				have = append(have, s.Text())
 			}
-			took := time.Since(readBegin)
-			t.Logf(
-				"Read %d record(s), %d byte(s) in %s -- %.2f MBps",
-				count, bytes, took,
-				float64(bytes/1024/1024)/took.Seconds(),
-			)
-		}(res.Records)
-	}
-	wg.Wait()
 
-	t.Logf("Took %s", time.Since(overallBegin))
+			// Make sure we got what we want!
+			if want := testcase.want; !reflect.DeepEqual(want, have) {
+				t.Fatalf("want %v, have %v", want, have)
+			}
+		})
+	}
+}
+
+// NOTE(tsenart): Profiling the benchmark with already generated test data
+// yields more meaningful and easy to understand results.
+//
+//   go test -c ./pkg/store
+//   ./store.test -test.run=XXX -test.benchmem -test.cpuprofile=out -test.bench=MergeReader
+//   go tool pprof -web store.test out
+//
+func BenchmarkMergeReader(b *testing.B) {
+	const size = 32 * 1024 * 1024
+	r, err := newMergeReader(generateSegments(b, 128, size, "testdata/segments"))
+	if err != nil {
+		b.Fatal(err)
+	}
+	p := make([]byte, 4096)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.Read(p)
+	}
+}
+
+func generateSegments(b testing.TB, count, size int, datadir string) (segs []io.Reader) {
+	if err := os.MkdirAll(datadir, 0755); err != nil {
+		b.Fatal(err)
+	}
+	matches, err := filepath.Glob(filepath.Join(datadir, "*.segment"))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for _, segment := range matches {
+		f, err := os.Open(segment)
+		if err != nil {
+			b.Fatal(err)
+		}
+		segs = append(segs, f)
+	}
+	if len(segs) > 0 {
+		return segs
+	}
+
+	ts := uint64(0)
+	body := append(bytes.Repeat([]byte("0"), 512), '\n')
+	record := make([]byte, ulid.EncodedSize+1+len(body))
+	record[ulid.EncodedSize] = ' '
+	copy(record[ulid.EncodedSize+1:], body)
+
+	buf := bytes.NewBuffer(make([]byte, 0, size))
+	for i := 0; i < count; i++ {
+		from := ulid.MustNew(ts, nil)
+		hi := ts
+		for buf.Len() < size {
+			ulid.MustNew(hi, nil).MarshalTextTo(record[:ulid.EncodedSize])
+			_, err := buf.Write(record)
+			if err != nil {
+				b.Fatal(err)
+			}
+			hi++
+		}
+
+		to := ulid.MustNew(hi, nil)
+		name := filepath.Join(datadir, fmt.Sprintf("%s-%s.segment", from, to))
+		f, err := os.Create(name)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		if _, err = buf.WriteTo(f); err != nil {
+			b.Fatal(err)
+		}
+
+		buf.Reset()
+		segs = append(segs, f)
+		ts = hi
+	}
+
+	return segs
+}
+
+func TestConcurrentFilteringReader(t *testing.T) {
+	t.Parallel()
+
+	var input bytes.Buffer
+	for i := 0; i < 5; i++ {
+		id := ulid.MustNew(uint64(i), nil)
+		fmt.Fprintln(&input, id, strconv.Itoa(i))
+	}
+
+	// Takes records from the filtering reader until EOF.
+	records := func(r io.Reader) []string {
+		have := []string{}
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			have = append(have, strings.Fields(s.Text())[1])
+		}
+		return have
+	}
+
+	for _, testcase := range []struct {
+		name     string
+		from, to ulid.ULID
+		q        string
+		want     []string
+	}{
+		{
+			name: "all",
+			from: ulid.MustNew(0, nil),
+			to:   ulid.MustNew(ulid.MaxTime(), nil),
+			want: []string{"0", "1", "2", "3", "4"},
+		},
+		{
+			name: "some",
+			from: ulid.MustNew(1, nil),
+			to:   ulid.MustNew(3, nil),
+			want: []string{"1", "2", "3"},
+		},
+		{
+			name: "some matching query",
+			from: ulid.MustNew(1, nil),
+			to:   ulid.MustNew(3, nil),
+			q:    "1|3",
+			want: []string{"1", "3"},
+		},
+		{
+			name: "none",
+			from: ulid.MustNew(0, nil),
+			to:   ulid.MustNew(ulid.MaxTime(), nil),
+			q:    "6",
+			want: []string{},
+		},
+		{
+			name: "query doesn't match record ids",
+			from: ulid.MustNew(0, nil),
+			to:   ulid.MustNew(ulid.MaxTime(), nil),
+			q:    ulid.MustNew(0, nil).String(),
+			want: []string{},
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			in := bytes.NewReader(input.Bytes())
+			re := regexp.MustCompile(testcase.q)
+			pass := recordFilterRegex(testcase.from, testcase.to, re)
+			r := newConcurrentFilteringReader(in, pass)
+			if want, have := testcase.want, records(r); !reflect.DeepEqual(want, have) {
+				t.Errorf("want %v, have %v", want, have)
+			}
+		})
+	}
 }
