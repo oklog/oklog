689a25bde9a0a1d5888bc4961c56fd24cb2b31c7
Peter Bourgon <peter@bourgon.org>
2017-01-16 17:04:31 +0100

    Release prep

--- release.fish
+++ release.fish
@@ -17,13 +17,13 @@ git tag --annotate v$VERSION -m "Release v$VERSION"
 echo Be sure to: git push --tags
 echo
 
-set DISTDIR dist/$VERSION
+set DISTDIR dist/v$VERSION
 mkdir -p $DISTDIR
 
 for pair in linux/386 linux/amd64 linux/arm linux/arm64 darwin/amd64 dragonfly/amd64 freebsd/amd64 netbsd/amd64 openbsd/amd64
 	set GOOS   (echo $pair | cut -d'/' -f1)
 	set GOARCH (echo $pair | cut -d'/' -f2)
-	set BIN    $DISTDIR/oklog-$GOOS-$GOARCH
+	set BIN    $DISTDIR/oklog-$VERSION-$GOOS-$GOARCH
 	echo $BIN
 	env GOOS=$GOOS GOARCH=$GOARCH go build -o $BIN -ldflags="-X main.version=$VERSION" github.com/oklog/oklog/cmd/oklog
 end
