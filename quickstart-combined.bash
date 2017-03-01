ec4a881b3491ed9ffaf6da28efd7e759d75ea9cc
Peter Bourgon <peterbourgon@users.noreply.github.com>
2017-03-01 23:01:53 +0100

    quickstart-combined.bash: ¡¿Qúe es en tarnación?!

--- quickstart-combined.bash
+++ quickstart-combined.bash
@@ -11,11 +11,11 @@ do
     -ingest.durable            tcp://0.0.0.0:100${i}2 \
     -ingest.bulk               tcp://0.0.0.0:100${i}3 \
     -cluster                   tcp://0.0.0.0:100${i}9 \
-	-store.segment-target-size 1000000 \
-	-store.segment-retain      30m \
-	-store.segment-purge       5m  \
+    -store.segment-target-size 1000000 \
+    -store.segment-retain      30m \
+    -store.segment-purge       5m  \
     -ingest.path               data/ingest/${i} \
-	-store.path                data/store/${i} \
+    -store.path                data/store/${i} \
     $PEERS 2>&1 | sed -e "s/^/[$i] /" &
   PEERS="$PEERS -peer 127.0.0.1:100${i}9"
 done
