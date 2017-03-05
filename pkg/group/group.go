25dd30f56baf613cf13913343ac7e7804a71fb36
Peter Bourgon <peter@bourgon.org>
2017-03-05 16:35:35 +0100

    pkg/group: clarify error behavior; add examples

--- pkg/group/group.go
+++ pkg/group/group.go
@@ -6,7 +6,7 @@
 package group
 
 // Group collects actors (functions) and runs them concurrently.
-// When one actor (function) returns, the others are interrupted.
+// When one actor (function) returns, all actors are interrupted.
 // The zero value of a Group is useful.
 type Group struct {
 	actors []actor
@@ -16,48 +16,16 @@ type Group struct {
 // interrupt function. That is, if interrupt is invoked, execute should return.
 // Also, it must be safe to call interrupt even after execute has returned.
 //
-// To add a general processing function, you can use a cancel chan.
-//
-//    cancel := make(chan struct{})
-//    g.Add(func() error {
-//        select {
-//        case <-time.After(5 * time.Second):
-//            return errors.New("time elapsed")
-//        case <-cancel:
-//            return errors.New("canceled")
-//        }
-//    }, func(error) {
-//        close(cancel)
-//    })
-//
-// This is a lower-level version of the semantics provided by contexts. So, if
-// your actor goroutine respects contexts, then you can easily accomplish the
-// same thing that way.
-//
-//    ctx, cancel := context.WithCancel(context.Background())
-//    g.Add(func() error {
-//        return runUntilCanceled(ctx, foo, bar)
-//    }, func(error) {
-//        cancel()
-//    })
-//
-// To add an e.g. HTTP server, you'll need to provide an explicit listener, so
-// that it may be interrupted.
-//
-//    ln, _ := net.Listen("tcp", "0.0.0.0:8080")
-//    g.Add(func() error {
-//        return http.Serve(ln, http.NewServeMux())
-//    }, func(error) {
-//        ln.Close()
-//    })
-//
+// The first actor (function) to return interrupts all running actors.
+// The error is passed to the interrupt functions, and is returned by Run.
 func (g *Group) Add(execute func() error, interrupt func(error)) {
 	g.actors = append(g.actors, actor{execute, interrupt})
 }
 
 // Run all actors (functions) concurrently.
 // When the first actor returns, all others are interrupted.
-// Run only returns when all actors (functions) have exited.
+// Run only returns when all actors have exited.
+// Run returns the error returned by the first exiting actor.
 func (g *Group) Run() error {
 	if len(g.actors) == 0 {
 		return nil
@@ -71,15 +39,15 @@ func (g *Group) Run() error {
 		}(a)
 	}
 
-	// Wait for the first one to stop.
+	// Wait for the first actor to stop.
 	err := <-errors
 
-	// Signal all others to stop.
+	// Signal all actors to stop.
 	for _, a := range g.actors {
 		a.interrupt(err)
 	}
 
-	// Wait for them all to stop.
+	// Wait for all actors to stop.
 	for i := 1; i < cap(errors); i++ {
 		<-errors
 	}
