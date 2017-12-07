f7e5711b6732e3f544b632ef0f034acfc042c3fb
Peter Bourgon <peterbourgon@users.noreply.github.com>
2017-12-07 12:32:18 +0100

    Improve logs related to calculated cluster advertise addresses (#88)
    * pkg/cluster: improve CalculateAdvertiseAddress
    
    Do a bit more work here than memberlist; namely, if the addrs are given
    as hostnames, we try to resolve them to IPs. This is so we don't log
    potentially confusing messages, like "couldn't parse IP" when a hostname
    is given and would actually work just fine.
    
    * cmd/oklog: debug-log calculated advertise addr

--- pkg/cluster/advertise.go
+++ pkg/cluster/advertise.go
@@ -10,21 +10,39 @@ import (
 // CalculateAdvertiseAddress attempts to clone logic from deep within memberlist
 // (NetTransport.FinalAdvertiseAddr) in order to surface its conclusions to the
 // application, so we can provide more actionable error messages if the user has
-// inadvertantly misconfigured their cluster.
+// inadvertantly misconfigured their cluster. Note that the output from this
+// function is only ever logged, it's not used as part of program execution.
+//
+// We do a bit more work here than memberlist; namely, if the addrs are given as
+// hostnames, we try to resolve them to IPs. This is so we don't log potentially
+// confusing messages, like "couldn't parse IP" when a hostname is given and
+// would actually work just fine.
 //
 // https://github.com/hashicorp/memberlist/blob/022f081/net_transport.go#L126
 func CalculateAdvertiseAddress(bindAddr, advertiseAddr string) (net.IP, error) {
 	if advertiseAddr != "" {
+		// First try to parse as IP.
 		ip := net.ParseIP(advertiseAddr)
-		if ip == nil {
-			return nil, errors.Errorf("failed to parse advertise addr '%s'", advertiseAddr)
+		if ip != nil {
+			if ip4 := ip.To4(); ip4 != nil {
+				ip = ip4
+			}
+			return ip, nil
 		}
-		if ip4 := ip.To4(); ip4 != nil {
-			ip = ip4
+
+		// If that fails, try to resolve as hostname.
+		resolved, err := net.ResolveIPAddr("ip", advertiseAddr)
+		if err == nil {
+			if ip4 := resolved.IP.To4(); ip4 != nil {
+				resolved.IP = ip4
+			}
+			return resolved.IP, nil
 		}
-		return ip, nil
+
+		return nil, errors.Errorf("failed to parse advertise addr '%s'", advertiseAddr)
 	}
 
+	// No explicit advertise address, try to deduce one from bind address.
 	if bindAddr == "0.0.0.0" {
 		privateIP, err := sockaddr.GetPrivateIP()
 		if err != nil {
@@ -40,9 +58,20 @@ func CalculateAdvertiseAddress(bindAddr, advertiseAddr string) (net.IP, error) {
 		return ip, nil
 	}
 
+	// Try to parse the addr as an IP.
 	ip := net.ParseIP(bindAddr)
-	if ip == nil {
-		return nil, errors.Errorf("failed to parse bind addr '%s'", bindAddr)
+	if ip != nil {
+		return ip, nil
 	}
-	return ip, nil
+
+	// If that fails, try to resolve as hostname,
+	resolved, err := net.ResolveIPAddr("ip", bindAddr)
+	if err == nil {
+		if ip4 := resolved.IP.To4(); ip4 != nil {
+			resolved.IP = ip4
+		}
+		return resolved.IP, nil
+	}
+
+	return nil, errors.Errorf("failed to parse bind addr '%s'", bindAddr)
 }
