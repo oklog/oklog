e952a28e958ddbe6a748e6eb08d9e2a3b68e89fe
yellow chicks <cdh_cjx@163.com>
2018-02-09 02:10:42 +0800

    easily understand overlap method (#103)

--- pkg/store/overlap.go
+++ pkg/store/overlap.go
@@ -1,24 +1,21 @@
 package store
 
 import (
-	"bytes"
-
 	"github.com/oklog/ulid"
 )
 
 // overlap returns true if the range [a, b] overlaps with [c, d].
 func overlap(a, b, c, d ulid.ULID) bool {
-	as, bs, cs, ds := a[:], b[:], c[:], d[:]
-	if bytes.Compare(as, bs) > 0 {
-		as, bs = bs, as
+	// sort bound
+	if a.Compare(b) > 0 {
+		a, b = b, a
 	}
-	if bytes.Compare(cs, ds) > 0 {
-		cs, ds = ds, cs
+	if c.Compare(d) > 0 {
+		c, d = d, c
 	}
-	if bytes.Compare(as, cs) > 0 {
-		as, bs, cs, ds = cs, ds, as, bs
-	}
-	if bytes.Compare(bs, cs) < 0 {
+
+	// [a, b] ∩  [c, d] == nil, return false
+	if b.Compare(c) < 0 || d.Compare(a) < 0 {
 		return false
 	}
 	return true
