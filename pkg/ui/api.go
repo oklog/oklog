03eecf6a62abf0e01674d6ebb57e8d2bc1e56da0
Alexander Simmerl <a.simmerl@gmail.com>
2018-07-27 21:41:08 +0200

    ui: Serve index from template
    Clean up the path juggling for the ui files once and for all to support
    local development, embedded static files and mounting of `/ui` under
    different paths and domains.

--- pkg/ui/api.go
+++ pkg/ui/api.go
@@ -1,10 +1,27 @@
 package ui
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/go-kit/kit/log"
 )
+
+const tmplIndex = `<!DOCTYPE html>
+<html>
+  <head profile="http://www.w3.org/2005/10/profile">
+    <meta charset="utf-8">
+    <title>OK Log</title>
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans+Mono|Droid+Sans:400,700" rel="stylesheet">
+    <link href="styles/normalize.css" rel="stylesheet">
+    <link href="styles/store.css" rel="stylesheet">
+    <script src="scripts/oklog.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <script src="scripts/ports.js" type="text/javascript"></script>
+ </body>
+</html>`
 
 // API serves the ui API.
 type API struct {
@@ -21,5 +38,17 @@ func NewAPI(logger log.Logger, local bool) *API {
 }
 
 func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	http.FileServer(_escFS(a.local)).ServeHTTP(w, r)
+	var (
+		mux     = http.NewServeMux()
+		tplRoot = template.Must(template.New("root").Parse(tmplIndex))
+	)
+
+	mux.Handle("/scripts/", http.FileServer(_escFS(a.local)))
+	mux.Handle("/styles/", http.FileServer(_escFS(a.local)))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_ = tplRoot.Execute(w, nil)
+	})
+
+	mux.ServeHTTP(w, r)
 }
--- ui/scripts/ports.js
+++ ui/scripts/ports.js
@@ -1,5 +1,5 @@
 ; (function() {
-	var app = Elm.Main.fullscreen(),
+	var app = Elm.Main.fullscreen({ "now": (new Date()).getTime() }),
 		left = "",
 		scroll = false,
 		stream = null;
